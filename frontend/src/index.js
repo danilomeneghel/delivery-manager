faaa363f86a56d318d18ef5359d3c9521e543c66
danilo.meneghel <danilo.meneghel@gmail.com>
2020-09-16 16:15:07 -0300

    Adjustment directory path css in index

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './../public/css/mui-theme.css';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
