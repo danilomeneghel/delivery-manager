94e36d22538dee826578eb063662fc89734dd28b
danilo.meneghel <danilo.meneghel@gmail.com>
2020-09-16 13:21:09 -0300

    Update URL API Production

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,8 @@ import axios from "axios";
 import { getToken } from "./auth";
 
 const api = axios.create({
-    baseURL: window.location.protocol + '//' + window.location.hostname + ':3001'
+    //baseURL: window.location.protocol + '//' + window.location.hostname + ':3001'
+    baseURL: 'https://crud-nodejs-mongo-db.herokuapp.com' //API Production
 });
 
 api.interceptors.request.use(async config => {
